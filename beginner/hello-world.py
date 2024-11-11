df1baedf651c5339aefe0296b0c2f70fde809f83
ShadowNotDefined <shadownotdefined@gmail.com>
2024-11-11 12:49:43 -0600

    fixed a typo

--- beginner/hello-world.py
+++ beginner/hello-world.py
@@ -1,4 +1,4 @@
-# THis is a comment, not code. You can use comments as little notes, like this.
+# This is a comment, not code. You can use comments as little notes, like this.
 
 # This line of code uses the print() function to print the string "Hello World!"
 print("Hello World!")
